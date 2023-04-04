746d1ad65c3a27dfc3d5d027ea5dd2a7a8d5202b
dogukanakkan <dogukanakkan3@gmail.com>
2023-04-04 14:27:57 +0300

    Initial commit

---
+++ src/ArtikYil.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ArtikYil {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen bir yıl giriniz: ");
+        int yil = input.nextInt();
+
+        if ((yil % 4 == 0 && yil % 100 != 0) || yil % 400 == 0) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
+
+    }
+}
